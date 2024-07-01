21d6f931580bc183d2b5879b0fab6b7f3d2915ee
Tikei <radovac.tikei@gmail.com>
2024-07-01 14:39:21 +0200

    first commit

---
+++ src/main/java/com/dinitProject/healthcheck_monitoring_app/HealthcheckMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.dinitProject.healthcheck_monitoring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcheckMonitoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcheckMonitoringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinitProject/healthcheck_monitoring_app/HealthcheckMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinitProject.healthcheck_monitoring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcheckMonitoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
