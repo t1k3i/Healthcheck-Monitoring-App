2a583f95a1a689a3454d9439cd36634abc20f374
Tikei <radovac.tikei@gmail.com>
2024-07-12 11:06:06 +0200

    Config class for cors

---
+++ src/main/java/com/dinit/healthcheck/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.dinit.healthcheck.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // Allow Angular's origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allowed headers
+                        .allowCredentials(true); // Allow credentials (if needed)
+            }
+        };
+    }
+
+}
